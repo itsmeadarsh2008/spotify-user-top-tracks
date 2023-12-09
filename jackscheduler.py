4ea23d13f0928858f688c75a21b283406a9a9ed0
Adarsh Gourab Mahalik <71959210+itsmeadarsh2008@users.noreply.github.com>
2023-12-09 10:01:58 +0000

    Workflows

--- jackscheduler.py
+++
@@ -1,9 +0,0 @@
-from main import run
-import time
-import schedule
-
-schedule.every().day.at("9:00").do(run)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
---
+++ runner.py
@@ -0,0 +1,9 @@
+from main import run
+# import time
+# import schedule
+
+# schedule.every().day.at("9:00").do(run)
+run()
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
